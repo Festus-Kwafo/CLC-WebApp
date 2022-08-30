8d864e3edfd139de91ba1fedacd9ea06d61c0108
Festus-Kwafo <festuskwafo11@gmail.com>
2022-08-30 03:05:59 +0000

    .env change

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import environ
-
+import os
 env = environ.Env()
 environ.Env.read_env()
 
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
