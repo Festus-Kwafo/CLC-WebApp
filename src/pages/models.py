6642d6f821f7a36bbce9849c6b3abe6d4dfef991
Festus Kwafo Ofori <68119922+Festus-Kwafo@users.noreply.github.com>
2023-12-06 15:36:21 +0000

    event model

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    one_day = models.BooleanField(default=False)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('event', args=[str(self.id)])
+
+    def get_date_range(self):
+        return self.start_date.strftime('%b %d') + ' - ' + self.end_date.strftime('%b %d, %Y')
