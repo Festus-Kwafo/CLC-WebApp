1e3f2fb4f897f1882ffda565d21531116423528f
Festus Kwafo Ofori <68119922+Festus-Kwafo@users.noreply.github.com>
2023-12-12 18:38:55 +0000

    storages bug fixed

--- src/core/settings.py
+++ src/core/settings.py
@@ -31,8 +31,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_prometheus',
-    'pages',
     'storages',
+    'pages',
+    
 ]
 
 MIDDLEWARE = [
